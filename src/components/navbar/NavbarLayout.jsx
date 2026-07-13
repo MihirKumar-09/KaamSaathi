a29128e47964632a0ab4a4b705729aec84050323
MihirKumar <mihirmalik34@gmail.com>
2026-07-13 13:01:52 +0530

    Create basic responsive structure of Navbar

--- src/components/navbar/NavbarLayout.jsx
+++ src/components/navbar/NavbarLayout.jsx
@@ -1,5 +1,8 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { Menu } from "lucide-react";
+import { useState } from "react";
 
 const workerLinks = [
   { id: 1, name: "Home", href: "/worker/dashboard" },
@@ -17,33 +20,50 @@ const employerLinks = [
   { id: 5, name: "Contact Us", href: "/contact" },
 ];
 export default function NavbarLayout() {
+  const [isOpen, setIsOpen] = useState(false);
   // Temporary for testing;
   const role = "worker";
   const navLinks = role === "worker" ? workerLinks : employerLinks;
   return (
-    <div className="flex items-center justify-between w-full h-20 px-6 lg:px-12 bg-white/80 backdrop-blur-md border-b border-gray-200 shadow-md sticky top-0 z-50">
-      <Link href={`/${role}/dashboard`}>
+    <>
+      <div className="flex items-center justify-between w-full h-20 px-6 lg:px-12 bg-white/80 backdrop-blur-md border-b border-gray-200 shadow-md sticky top-0 z-50">
+        <Link href={`/${role}/dashboard`}>
+          <div>
+            <Image
+              src="/logo/logo.png"
+              alt="KaamSaathi Logo"
+              height={80}
+              width={80}
+            />
+          </div>
+        </Link>
         <div>
-          <Image
-            src="/logo/logo.png"
-            alt="KaamSaathi Logo"
-            height={80}
-            width={80}
-          />
+          <ul className="hidden lg:flex gap-12 font-medium ">
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className="hover:text-green-600 transition-all duration-300"
+              >
+                <Link href={link.href}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
-      </Link>
-      <div>
-        <ul className="flex gap-12 font-medium ">
-          {navLinks.map((link) => (
-            <li
-              key={link.id}
-              className="hover:text-green-600 transition-all duration-300"
-            >
-              <Link href={link.href}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
+        <button onClick={() => setIsOpen(!isOpen)} className="lg:hidden">
+          <Menu size={28} />
+        </button>
       </div>
-    </div>
+      {isOpen && (
+        <div className="bg-white">
+          <ul>
+            {navLinks.map((link) => (
+              <li key={link.id}>
+                <Link href={link.href}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
