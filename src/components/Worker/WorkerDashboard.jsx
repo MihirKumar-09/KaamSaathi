08ca19578f82ff2359a9550aa6ad196264490afa
MihirKumar <mihirmalik34@gmail.com>
2026-07-14 10:48:45 +0530

    Set up basic layout for footer

--- src/components/Worker/WorkerDashboard.jsx
+++ src/components/Worker/WorkerDashboard.jsx
@@ -1,8 +1,10 @@
 import NavbarLayout from "../navbar/NavbarLayout";
+import FooterLayout from "../footer/FooterLayout";
 export default function WorkerDashboard() {
   return (
     <div>
       <NavbarLayout />
+      <FooterLayout />
     </div>
   );
 }
---
+++ src/components/footer/FooterLayout.jsx
@@ -0,0 +1,7 @@
+export default function FooterLayout() {
+  return (
+    <div>
+      <h2>Footer Layout</h2>
+    </div>
+  );
+}
